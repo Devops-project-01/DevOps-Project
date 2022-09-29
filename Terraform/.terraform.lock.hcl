53238ecf7734fe7ea036b6759147049efb435b63
Naman <namanjain7206@gmail.com>
2022-09-29 14:53:13 +0000

    Connecting Terraform with ansible Completed

--- Terraform/.terraform.lock.hcl
+++ Terraform/.terraform.lock.hcl
@@ -19,3 +19,41 @@ provider "registry.terraform.io/hashicorp/google" {
     "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
   ]
 }
+
+provider "registry.terraform.io/hashicorp/local" {
+  version = "2.2.3"
+  hashes = [
+    "h1:aWp5iSUxBGgPv1UnV5yag9Pb0N+U1I0sZb38AXBFO8A=",
+    "zh:04f0978bb3e052707b8e82e46780c371ac1c66b689b4a23bbc2f58865ab7d5c0",
+    "zh:6484f1b3e9e3771eb7cc8e8bab8b35f939a55d550b3f4fb2ab141a24269ee6aa",
+    "zh:78a56d59a013cb0f7eb1c92815d6eb5cf07f8b5f0ae20b96d049e73db915b238",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:8aa9950f4c4db37239bcb62e19910c49e47043f6c8587e5b0396619923657797",
+    "zh:996beea85f9084a725ff0e6473a4594deb5266727c5f56e9c1c7c62ded6addbb",
+    "zh:9a7ef7a21f48fabfd145b2e2a4240ca57517ad155017e86a30860d7c0c109de3",
+    "zh:a63e70ac052aa25120113bcddd50c1f3cfe61f681a93a50cea5595a4b2cc3e1c",
+    "zh:a6e8d46f94108e049ad85dbed60354236dc0b9b5ec8eabe01c4580280a43d3b8",
+    "zh:bb112ce7efbfcfa0e65ed97fa245ef348e0fd5bfa5a7e4ab2091a9bd469f0a9e",
+    "zh:d7bec0da5c094c6955efed100f3fe22fca8866859f87c025be1760feb174d6d9",
+    "zh:fb9f271b72094d07cef8154cd3d50e9aa818a0ea39130bc193132ad7b23076fd",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/tls" {
+  version     = "3.1.0"
+  constraints = "3.1.0"
+  hashes = [
+    "h1:fUJX8Zxx38e2kBln+zWr1Tl41X+OuiE++REjrEyiOM4=",
+    "zh:3d46616b41fea215566f4a957b6d3a1aa43f1f75c26776d72a98bdba79439db6",
+    "zh:623a203817a6dafa86f1b4141b645159e07ec418c82fe40acd4d2a27543cbaa2",
+    "zh:668217e78b210a6572e7b0ecb4134a6781cc4d738f4f5d09eb756085b082592e",
+    "zh:95354df03710691773c8f50a32e31fca25f124b7f3d6078265fdf3c4e1384dca",
+    "zh:9f97ab190380430d57392303e3f36f4f7835c74ea83276baa98d6b9a997c3698",
+    "zh:a16f0bab665f8d933e95ca055b9c8d5707f1a0dd8c8ecca6c13091f40dc1e99d",
+    "zh:be274d5008c24dc0d6540c19e22dbb31ee6bfdd0b2cddd4d97f3cd8a8d657841",
+    "zh:d5faa9dce0a5fc9d26b2463cea5be35f8586ab75030e7fa4d4920cd73ee26989",
+    "zh:e9b672210b7fb410780e7b429975adcc76dd557738ecc7c890ea18942eb321a5",
+    "zh:eb1f8368573d2370605d6dbf60f9aaa5b64e55741d96b5fb026dbfe91de67c0d",
+    "zh:fc1e12b713837b85daf6c3bb703d7795eaf1c5177aebae1afcf811dd7009f4b0",
+  ]
+}
--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,28 +1,145 @@
+terraform {
+  required_providers {
+    tls = {
+      source  = "hashicorp/tls"
+      version = "3.1.0"
+    }
+  }
+}
+
+provider "tls" {
+  // no config needed
+}
+
+resource "tls_private_key" "ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "ssh_private_key_pem" {
+  content         = tls_private_key.ssh.private_key_pem
+  filename        = "../../.ssh/google_compute_engine"
+  file_permission = "0600"
+}
+
+resource "local_file" "ssh_public_key_pem" {
+  content         = tls_private_key.ssh.public_key_openssh
+  filename        = "../../.ssh/google_compute_engine.pub"
+  file_permission = "0600"
+}
+resource "local_file" "authorized_key_pem" {
+  content         = tls_private_key.ssh.public_key_openssh
+  filename        = "../../.ssh/authorized_keys"
+  file_permission = "0600"
+}
+
 provider "google" {
   project = "gcp-testo1"
   region  = "us-central1"
   zone    = "us-central1-c"
 }
 
+resource "google_project_service" "cloud_resource_manager" {
+  service            = "cloudresourcemanager.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "compute" {
+  service            = "compute.googleapis.com"
+  disable_on_destroy = false
+}
+resource "google_compute_address" "static_ip" {
+  name = "terraform-created-vm"
+}
+
+data "google_client_openid_userinfo" "me" {}
+
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance-1"
   machine_type = "e2-micro"
-
+  tags = ["allow-ssh"] 
   boot_disk {
     initialize_params {
       image = "ubuntu-os-pro-cloud/ubuntu-pro-2004-lts"
     }
-  }
+}
+  metadata = {
+   # ssh-keys = "${split("@", data.google_client_openid_userinfo.me.email)[0]}:${tls_private_key.ssh.public_key_openssh}"
+ ssh-keys = "ansible:${tls_private_key.ssh.public_key_openssh}" 
+}
 
-  network_interface {
+ network_interface {
     # A default network is created for all GCP projects
-    network = google_compute_network.vpc_network.self_link
+    network = google_compute_network.custom-test.self_link
+    subnetwork = google_compute_subnetwork.network-1.self_link
     access_config {
+	nat_ip = google_compute_address.static_ip.address
     }
   }
 }
+resource "google_compute_instance" "vm_instance-2" {
+  name         = "terraform-instance-2"
+  machine_type = "e2-micro"
+  tags = ["allow-ssh"]
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-pro-cloud/ubuntu-pro-2004-lts"
+    }
+}
+  metadata = {
+   # ssh-keys = "${split("@", data.google_client_openid_userinfo.me.email)[0]}:${tls_private_key.ssh.public_ke>
+ ssh-keys = "ansible:${tls_private_key.ssh.public_key_openssh}"
+}
 
-resource "google_compute_network" "vpc_network" {
-  name                    = "terraform-network"
-  auto_create_subnetworks = "true"
+ network_interface {
+    # A default network is created for all GCP projects
+    network = google_compute_network.custom-test.self_link
+    subnetwork = google_compute_subnetwork.network-1.self_link
+    access_config {
+            }
+  }
+}
+
+resource "google_compute_subnetwork" "network-1" {
+  name          = "test-subnetwork"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.custom-test.id
+}
+
+resource "google_compute_firewall" "ansible-fw-1" {
+  name    = "test-firewall"
+  network = google_compute_network.custom-test.name
+  target_tags   = ["allow-ssh"] 
+  source_ranges = ["0.0.0.0/0"]
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "8080", "22"]
+  }
+}
+
+resource "google_compute_network" "custom-test" {
+  name                    = "test-network"
+  auto_create_subnetworks = false
+}
+
+resource "local_file" "inventory" {
+ filename = "./../../inventory/hosts.ini"
+ content = <<EOF
+[webserver]
+${google_compute_instance.vm_instance.network_interface.0.network_ip}
+localhost
+${google_compute_instance.vm_instance-2.network_interface.0.access_config.0.nat_ip}
+[remote]
+${google_compute_instance.vm_instance.network_interface.0.access_config.0.nat_ip}
+${google_compute_instance.vm_instance-2.network_interface.0.access_config.0.nat_ip}
+EOF
+}
+
+output "public_ip" {
+  value = google_compute_address.static_ip.address
 }
